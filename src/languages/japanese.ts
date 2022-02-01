6d31b46997c433ac7faa8d6af1c6a9d724e47986
Andrew Huntington <andrew.huntington@gmail.com>
2022-02-01 19:35:49 +0900

    Improve Japanese text. Update description meta tag."

--- src/languages/japanese.ts
+++ src/languages/japanese.ts
@@ -3,7 +3,7 @@ export const japanese: Record<string, string> = {
   job: 'フロントエンド開発者',
   about: '私について',
   aboutContent:
-    '熱心に技術を学ぶことを好み、フロントエンド開発者として活動しています。フロントエンド開発者としては、React、TypeScript、Tailwind CSS、Git、Visual Studio Codeなどの良いツールを使用しています。目標はプロジェクトに参加しているごとに新しいことを学びます。現在、素敵な港湾都市、横浜で生活しています。',
+    '熱心に技術を学ぶことを好み、フロントエンド開発者として活動していて、発想を具体化させることに熱意を持っています。フロントエンド開発者としては、React、TypeScript、Tailwind CSS、Git、Visual Studio Codeなどのツールを好んで使用しています。目標はプロジェクトに参加するごとに新しいことを学ぶことです。現在、素敵な港湾都市、横浜で生活しています。',
   interests: '趣味',
   interestsContent:
     '音楽鑑賞、読書、ビデオゲーム、コンピュータプログラミング、日本語、スペイン語、翻訳。',
